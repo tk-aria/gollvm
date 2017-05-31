6a9a11c72ab2ab469068d4310530cb95404432e2
Than McIntosh <thanm@google.com>
2017-05-31 09:23:17 -0400

    gollvm: pairvalmap code cleanup
    Split out the templatized 'pairvalmap' helper class into a
    seperate header in preparation for using it elsewhere in the
    bridge. Clean up some code cruft in go-llvm.h. No changes to
    functionality.
    
    Change-Id: If232e653e514923cb7eba6819a27e8ecb00f0ff3
    Reviewed-on: https://go-review.googlesource.com/44431
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

---
+++ llvm-gofrontend/go-llvm-maptypes.h
@@ -0,0 +1,44 @@
+//===-- go-llvm-maptypes.h - hashmap type declarations --------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Templated type declarations for hashmap helper classes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVMGOFRONTEND_GO_LLVM_MAPTYPES_H
+#define LLVMGOFRONTEND_GO_LLVM_MAPTYPES_H
+
+#include <unordered_map>
+
+// In various bridge classes it is useful to keep tables that incorporate
+// a key of pair type <X,Y> and with some arbitrary value type. Set up a
+// templatized class 'pairvalmap' to help with this.
+
+template <typename T1, typename T2> struct pairvalmap_hash {
+  typedef std::pair<T1, T2> pairtype;
+  unsigned int operator()(const pairtype &p) const {
+    std::size_t h1 = std::hash<T1>{}(p.first);
+    std::size_t h2 = std::hash<T2>{}(p.second);
+    return h1 + h2;
+  }
+};
+
+template <typename T1, typename T2> struct pairvalmap_equal {
+  typedef std::pair<T1, T2> pairtype;
+  bool operator()(const pairtype &p1, const pairtype &p2) const {
+    return (p1.first == p2.first && p1.second == p2.second);
+  }
+};
+
+template <typename T1, typename T2, typename V>
+using pairvalmap =
+    std::unordered_map<std::pair<T1, T2>, V, pairvalmap_hash<T1, T2>,
+                       pairvalmap_equal<T1, T2>>;
+
+#endif // LLVMGOFRONTEND_GO_LLVM_MAPTYPES_H
--- llvm-gofrontend/go-llvm-typemanager.h
+++ llvm-gofrontend/go-llvm-typemanager.h
@@ -251,27 +251,6 @@ class TypeManager {
   typedef std::unordered_map<Btype *, llvm::Type *> pproxymap;
   llvm::Type *placeholderProxyType(Btype *typ, pproxymap *pmap);
 
-  template <typename T1, typename T2> struct pairvalmap_hash {
-    typedef std::pair<T1, T2> pairtype;
-    unsigned int operator()(const pairtype &p) const {
-      std::size_t h1 = std::hash<T1>{}(p.first);
-      std::size_t h2 = std::hash<T2>{}(p.second);
-      return h1 + h2;
-    }
-  };
-
-  template <typename T1, typename T2> struct pairvalmap_equal {
-    typedef std::pair<T1, T2> pairtype;
-    bool operator()(const pairtype &p1, const pairtype &p2) const {
-      return (p1.first == p2.first && p1.second == p2.second);
-    }
-  };
-
-  template <typename T1, typename T2, typename V>
-  using pairvalmap =
-      std::unordered_map<std::pair<T1, T2>, V, pairvalmap_hash<T1, T2>,
-                         pairvalmap_equal<T1, T2>>;
-
   // Context information needed for the LLVM backend.
   llvm::LLVMContext &context_;
   const llvm::DataLayout *datalayout_;
