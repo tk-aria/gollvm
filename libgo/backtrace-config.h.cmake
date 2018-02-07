2ae1e1106ca481f203cdff039f6d510da12e9623
Cherry Zhang <cherryyz@google.com>
2018-02-07 17:43:23 -0500

    gollvm: use #cmakedefine in libbacktrace config
    libbacktrace tests its configurations as defined vs. not defined,
    instead of 0/1. Fix the cmake config file.
    
    Change-Id: Ie1de5359fef425ce2d8bc4e087c45a68b9dd90af
    Reviewed-on: https://go-review.googlesource.com/92620
    Reviewed-by: Than McIntosh <thanm@google.com>

--- libgo/backtrace-config.h.cmake
+++ libgo/backtrace-config.h.cmake
@@ -7,81 +7,80 @@
 #cmakedefine BACKTRACE_XCOFF_SIZE
 
 /* Define to 1 if you have the __atomic functions */
-#cmakedefine01 HAVE_ATOMIC_FUNCTIONS
+#cmakedefine HAVE_ATOMIC_FUNCTIONS 1
 
 /* Define to 1 if you have the `clock_gettime' function. */
-#cmakedefine01 HAVE_CLOCK_GETTIME
+#cmakedefine HAVE_CLOCK_GETTIME 1
 
 /* Define to 1 if you have the declaration of `strnlen', and to 0 if you
    don't. */
-#cmakedefine01 HAVE_DECL_STRNLEN
+#cmakedefine HAVE_DECL_STRNLEN 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#cmakedefine01 HAVE_DLFCN_H
+#cmakedefine HAVE_DLFCN_H 1
 
 /* Define if dl_iterate_phdr is available. */
-#cmakedefine01 HAVE_DL_ITERATE_PHDR
+#cmakedefine HAVE_DL_ITERATE_PHDR 1
 
 /* Define to 1 if you have the fcntl function */
-#cmakedefine01 HAVE_FCNTL
+#cmakedefine HAVE_FCNTL 1
 
 /* Define if getexecname is available. */
-#cmakedefine01 HAVE_GETEXECNAME
+#cmakedefine HAVE_GETEXECNAME 1
 
 /* Define if _Unwind_GetIPInfo is available. */
-#cmakedefine01 HAVE_GETIPINFO
+#cmakedefine HAVE_GETIPINFO 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#cmakedefine01 HAVE_INTTYPES_H
+#cmakedefine HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `z' library (-lz). */
-#cmakedefine01 HAVE_LIBZ
+#cmakedefine HAVE_LIBZ 1
 
 /* Define to 1 if you have the <link.h> header file. */
-#cmakedefine01 HAVE_LINK_H
+#cmakedefine HAVE_LINK_H 1
 
 /* Define if AIX loadquery is available. */
-#cmakedefine01 HAVE_LOADQUERY
+#cmakedefine HAVE_LOADQUERY 1
 
 /* Define to 1 if you have the `lstat' function. */
-#cmakedefine01 HAVE_LSTAT
+#cmakedefine HAVE_LSTAT 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#cmakedefine01 HAVE_MEMORY_H
+#cmakedefine HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `readlink' function. */
-#cmakedefine01 HAVE_READLINK
+#cmakedefine HAVE_READLINK 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine01 HAVE_STDINT_H
+#cmakedefine HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#cmakedefine01 HAVE_STDLIB_H
+#cmakedefine HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#cmakedefine01 HAVE_STRINGS_H
+#cmakedefine HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#cmakedefine01 HAVE_STRING_H
+#cmakedefine HAVE_STRING_H 1
 
 /* Define to 1 if you have the __sync functions */
-#cmakedefine01 HAVE_SYNC_FUNCTIONS
+#cmakedefine HAVE_SYNC_FUNCTIONS 1
 
 /* Define to 1 if you have the <sys/ldr.h> header file. */
-#cmakedefine01 HAVE_SYS_LDR_H
+#cmakedefine HAVE_SYS_LDR_H 1
 
 /* Define to 1 if you have the <sys/mman.h> header file. */
-#cmakedefine01 HAVE_SYS_MMAN_H
+#cmakedefine HAVE_SYS_MMAN_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#cmakedefine01 HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#cmakedefine01 HAVE_SYS_TYPES_H
+#cmakedefine HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#cmakedefine01 HAVE_UNISTD_H
+#cmakedefine HAVE_UNISTD_H 1
 
 /* Define if -lz is available. */
-#cmakedefine01 HAVE_ZLIB
-
+#cmakedefine HAVE_ZLIB 1
