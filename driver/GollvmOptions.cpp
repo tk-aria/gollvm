f6a2a6eac2d724bec12050c0dd7e2fde2b95c7a1
Than McIntosh <thanm@google.com>
2018-04-04 08:48:38 -0400

    gollvm: new command line option-handling infrastructure
    Create base support for specifying command line options via tablegen
    definition file, in preparation for more comprehensive/complete driver
    options for gollvm. The definition file (GollvmOptions.td) contains
    definitions for a representative set of options accepted by gollvm,
    however for the moment the old llvm::cl style options handling is
    still in place (will be subsumed in subsequent patches).
    
    Also this patch gollvm now accepts low-level llvm::cl options for
    debugging LLVM passes via the "-mllvm" option, similar to clang. For
    example, to dump out machine instructions after the stack coloring
    pass:
    
      llvm-goc -O3 -c -g abc.go -mllvm -print-machineinstrs=stack-coloring
    
    There should be no actual changes in driver functionality with this change.
    
    Change-Id: If8563133d2d1ea139619e7a99b76a98ecec8ff2a

---
+++ driver/GollvmOptions.cpp
@@ -0,0 +1,49 @@
+//===--- GollvmOptions.cpp - Gollvm Driver Options Table ------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "GollvmOptions.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Option/OptTable.h"
+#include "llvm/Option/Option.h"
+
+using namespace llvm::opt;
+
+namespace gollvm {
+namespace options {
+
+#define PREFIX(NAME, VALUE) static const char *const NAME[] = VALUE;
+#include "GollvmOptions.inc"
+#undef PREFIX
+
+static const OptTable::Info InfoTable[] = {
+#define OPTION(PREFIX, NAME, ID, KIND, GROUP, ALIAS, ALIASARGS, FLAGS, PARAM,  \
+               HELPTEXT, METAVAR, VALUES)                                      \
+  {PREFIX, NAME,  HELPTEXT,    METAVAR,     OPT_##ID,  Option::KIND##Class,    \
+   PARAM,  FLAGS, OPT_##GROUP, OPT_##ALIAS, ALIASARGS, VALUES},
+#include "GollvmOptions.inc"
+#undef OPTION
+};
+
+namespace {
+
+class DriverOptTable : public OptTable {
+public:
+  DriverOptTable()
+      : OptTable(InfoTable) {}
+};
+
+}
+
+std::unique_ptr<OptTable> createGollvmDriverOptTable() {
+  auto Result = llvm::make_unique<DriverOptTable>();
+  return std::move(Result);
+}
+
+}
+}
