55c7dbf12dbb8d6d394012c0ec3e52e5ff5cb4ec
Than McIntosh <thanm@google.com>
2018-04-04 08:48:38 -0400

    gollvm: new command line option-handling infrastructure
    Create base support for specifying command line options via tablegen
    definition file, in preparation for more comprehensive/complete driver
    options for gollvm. The definition file (GollvmOptions.td) contains
    definitions for a representative set of options accepted by gollvm,
    however for the moment the old llvm::cl style options handling is
    still in place (will be subsumed in subsequent patches).
    
    Also this patch gollvm now accepts low-level llvm::cl options for
    debugging LLVM passes via the "-mllvm" option, similar to clang. For
    example, to dump out machine instructions after the stack coloring
    pass:
    
      llvm-goc -O3 -c -g abc.go -mllvm -print-machineinstrs=stack-coloring
    
    There should be no actual changes in driver functionality with this change.
    
    Change-Id: If8563133d2d1ea139619e7a99b76a98ecec8ff2a

---
+++ driver/GollvmOptions.h
@@ -0,0 +1,51 @@
+//===--- GollvmOptions.h - Gollvm Option info & table -----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_GOLLVM_DRIVER_GOLLVMOPTIONS_H
+#define LLVM_GOLLVM_DRIVER_GOLLVMOPTIONS_H
+
+#include <memory>
+
+namespace llvm {
+namespace opt {
+class OptTable;
+}
+}
+
+namespace gollvm {
+namespace options {
+
+/// Flags specifically for gollvm options.  Must not overlap with
+/// llvm::opt::DriverFlag.
+enum flags {
+  DriverOption = (1 << 4),
+  LinkerInput = (1 << 5),
+  NoArgumentUnused = (1 << 6),
+  Unsupported = (1 << 7),
+  CoreOption = (1 << 8),
+  NoDriverOption = (1 << 9),
+  Ignored = (1 << 10)
+};
+
+enum ID {
+    OPT_INVALID = 0, // This is not an option ID.
+#define OPTION(PREFIX, NAME, ID, KIND, GROUP, ALIAS, ALIASARGS, FLAGS, PARAM,  \
+               HELPTEXT, METAVAR, VALUES)                                      \
+  OPT_##ID,
+#include "GollvmOptions.inc"
+    LastOption
+#undef OPTION
+};
+
+extern std::unique_ptr<llvm::opt::OptTable> createGollvmDriverOptTable();
+
+} // end namespace options
+} // end namespace gollvm
+
+#endif
