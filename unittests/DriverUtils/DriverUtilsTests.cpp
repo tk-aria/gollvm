d01f2a5601d2b2cd638cc23186998f5d3c6aeb8f
Than McIntosh <thanm@google.com>
2018-04-23 14:52:17 -0400

    gollvm: driver code reorganization
    Split the existing driver code into two subdirectories, one with a
    driver utilities library and one with the driver executable itself
    (which links against the utilities library). This is intended to make
    it possible write unit tests for code in the utilities library. Add a
    stub driver utilities unit test (contents to be added later).
    
    Change-Id: I9e3841b85bae10c918552cb3ef70297243ae970d

---
+++ unittests/DriverUtils/DriverUtilsTests.cpp
@@ -0,0 +1,30 @@
+//===---- DriverUtilsTests.cpp --------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <iostream>
+#include <map>
+#include <set>
+
+// nothing here yet (stub)
+
+#include "gtest/gtest.h"
+
+#include "DiffUtils.h"
+
+using namespace goBackendUnitTests;
+
+namespace {
+
+TEST(DriverUtilsTests, Empty) {
+
+  // To be filled in later.
+
+}
+
+} // namespace
