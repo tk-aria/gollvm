144e75901bb19bd0ef3612d43a7cbcd0a47af081
Than McIntosh <thanm@google.com>
2018-04-24 11:48:25 -0400

    gollvm: refactor command line parser in driver
    Separate out command line parsing from the compiler orchestrator
    class, in preparation for switching over to the new Driver class.
    No change in compiler functionality.
    
    Change-Id: I033027b49354942d375c60e4cfdb56b6646dff2a

--- driver-main/llvm-goc.cpp
+++ driver-main/llvm-goc.cpp
@@ -25,7 +25,6 @@
 #include "ToolChain.h"
 #include "Tool.h"
 
-
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/Triple.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
@@ -69,6 +68,90 @@
 #include <unistd.h>
 
 using namespace llvm;
+
+class CommandLineParser {
+ public:
+  CommandLineParser(opt::OptTable *opts)
+      : opts_(opts)
+  { }
+
+  bool parseCommandLine(int argc, char **argv);
+
+  opt::InputArgList &args() { return args_; }
+
+ private:
+  opt::OptTable *opts_;
+  opt::InputArgList args_;
+};
+
+bool CommandLineParser::parseCommandLine(int argc, char **argv)
+{
+  const char *progname = argv[0];
+
+  unsigned missingArgIndex, missingArgCount;
+  ArrayRef<const char *> argvv = makeArrayRef(argv, argc);
+  args_ = opts_->ParseArgs(argvv.slice(1), missingArgIndex, missingArgCount);
+
+  // Honor --help first
+  if (args_.hasArg(gollvm::options::OPT_help)) {
+    opts_->PrintHelp(errs(), progname, "Gollvm (LLVM-based Go compiler)",
+                     0, 0, false);
+    exit(0);
+  }
+
+  // Complain about missing arguments.
+  if (missingArgIndex != 0) {
+    errs() << progname << ": error: argument to '"
+           << args_.getArgString(missingArgIndex)
+           << "' option missing, espected "
+           << missingArgCount << " value(s)\n";
+    return false;
+  }
+
+  // Check for unsupported options.
+  for (const opt::Arg *arg : args_) {
+    if (arg->getOption().hasFlag(gollvm::options::Unsupported)) {
+      errs() << progname << ": error: unsupported command line option '"
+             << arg->getAsString(args_) << "'\n";
+      return false;
+    }
+  }
+
+  // Check for unknown options.
+  bool foundUnknown = false;
+  for (const opt::Arg *arg : args_.filtered(gollvm::options::OPT_UNKNOWN)) {
+    errs() << progname << ": error: unrecognized command line option '"
+             << arg->getAsString(args_) << "'\n";
+    foundUnknown = true;
+  }
+  if (foundUnknown)
+    return false;
+
+  // Honor -mllvm
+  auto llvmargs = args_.getAllArgValues(gollvm::options::OPT_mllvm);
+  if (! llvmargs.empty()) {
+    unsigned nargs = llvmargs.size();
+    auto args = llvm::make_unique<const char*[]>(nargs + 2);
+    args[0] = "gollvm (LLVM option parsing)";
+    for (unsigned i = 0; i != nargs; ++i)
+      args[i + 1] = llvmargs[i].c_str();
+    args[nargs + 1] = nullptr;
+    llvm::cl::ParseCommandLineOptions(nargs + 1, args.get());
+  }
+
+  // Vett the -x option if present. At the moment only "-x go" is accepted
+  // (assembler not allowed, but it could conceivably be added later).
+  opt::Arg *xarg = args_.getLastArg(gollvm::options::OPT_x);
+  if (xarg != nullptr &&
+      ! llvm::StringRef(xarg->getValue()).equals("go")) {
+    errs() << progname << ": invalid argument '"
+           << xarg->getValue() << "' to '"
+           << xarg->getAsString(args_) << "' option\n";
+    return false;
+  }
+
+  return true;
+}
 
 static std::unique_ptr<ToolOutputFile>
 GetOutputStream(std::string outFileName, bool binary)
@@ -120,13 +203,15 @@ enum CompileMode {
 
 class CompilationOrchestrator {
  public:
-  CompilationOrchestrator(const char *argvZero);
+  CompilationOrchestrator(const char *argvZero,
+                          opt::InputArgList &args,
+                          opt::OptTable &opts);
   ~CompilationOrchestrator();
 
   // Various stages in the setup/execution of the compilation.  The
   // convention here is to return false if there was a fatal error, true
   // otherwise.
-  bool parseCommandLine(int argc, char **argv);
+  bool inspectCommandLine();
   bool preamble();
   bool initBridge();
   bool resolveInputOutput();
@@ -145,10 +230,8 @@ class CompilationOrchestrator {
   CodeGenOpt::Level cgolvl_;
   unsigned olvl_;
   int errorReturnCode_;
-  std::unique_ptr<opt::OptTable> opts_;
-  SmallVector<const char *, 256> argvsmv_;
-  SpecificBumpPtrAllocator<char> argallocator_;
-  opt::InputArgList args_;
+  opt::OptTable &opts_;
+  opt::InputArgList &args_;
   std::unique_ptr<Llvm_backend> bridge_;
   std::unique_ptr<TargetMachine> target_;
   std::unique_ptr<Llvm_linemap> linemap_;
@@ -190,11 +273,11 @@ class CompilationOrchestrator {
   std::string firstFileBase();
 };
 
-CompilationOrchestrator::CompilationOrchestrator(const char *argvZero)
-
+CompilationOrchestrator::CompilationOrchestrator(const char *argvZero,
+                                                 opt::InputArgList &args,
+                                                 opt::OptTable &opts)
     : progname_(argvZero), cgolvl_(CodeGenOpt::Default),
-      olvl_(2), errorReturnCode_(1),
-      opts_(gollvm::options::createGollvmDriverOptTable()),
+      olvl_(2), errorReturnCode_(1), opts_(opts), args_(args),
       compileMode_(UnknownCompileMode),
       hasError_(false), tmpCreated_(false)
 {
@@ -321,61 +404,8 @@ std::string CompilationOrchestrator::firstFileBase()
   return firstFile.substr(0, dotindex);
 }
 
-bool CompilationOrchestrator::parseCommandLine(int argc, char **argv)
+bool CompilationOrchestrator::inspectCommandLine()
 {
-  unsigned missingArgIndex, missingArgCount;
-  ArrayRef<const char *> argvv = makeArrayRef(argv, argc);
-
-  this->args_ =
-      opts_->ParseArgs(argvv.slice(1), missingArgIndex, missingArgCount);
-
-  // Honor --help first
-  if (args_.hasArg(gollvm::options::OPT_help)) {
-    opts_->PrintHelp(errs(), progname_, "Gollvm (LLVM-based Go compiler)",
-                     0, 0, false);
-    exit(0);
-  }
-
-  // Complain about missing arguments.
-  if (missingArgIndex != 0) {
-    errs() << "error: argument to '"
-           << args_.getArgString(missingArgIndex)
-           << "' option missing, expected "
-           << missingArgCount << " value(s)\n";
-    return false;
-  }
-
-  // Check for unsupported options.
-  for (const opt::Arg *arg : args_) {
-    if (arg->getOption().hasFlag(gollvm::options::Unsupported)) {
-      errs() << "error: unsupported command line option '"
-             << arg->getAsString(args_) << "'\n";
-      return false;
-    }
-  }
-
-  // Check for unknown options.
-  bool foundUnknown = false;
-  for (const opt::Arg *arg : args_.filtered(gollvm::options::OPT_UNKNOWN)) {
-    errs() << "error: unrecognized command line option '"
-             << arg->getAsString(args_) << "'\n";
-    foundUnknown = true;
-  }
-  if (foundUnknown)
-    return false;
-
-  // Honor -mllvm
-  auto llvmargs = args_.getAllArgValues(gollvm::options::OPT_mllvm);
-  if (! llvmargs.empty()) {
-    unsigned nargs = llvmargs.size();
-    auto args = llvm::make_unique<const char*[]>(nargs + 2);
-    args[0] = "gollvm (LLVM option parsing)";
-    for (unsigned i = 0; i != nargs; ++i)
-      args[i + 1] = llvmargs[i].c_str();
-    args[nargs + 1] = nullptr;
-    llvm::cl::ParseCommandLineOptions(nargs + 1, args.get());
-  }
-
   // Collect input file names
   for (opt::Arg *arg : args_) {
     if (arg->getOption().getKind() == opt::Option::InputClass) {
@@ -982,18 +1012,25 @@ bool CompilationOrchestrator::invokeAssembler()
 
 int main(int argc, char **argv)
 {
-  CompilationOrchestrator orchestrator(argv[0]);
-
   // Print a stack trace if we signal out.
   sys::PrintStackTraceOnErrorSignal(argv[0]);
   PrettyStackTraceProgram X(argc, argv);
   llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
 
   // Parse command line.
-  if (!orchestrator.parseCommandLine(argc, argv))
+  std::unique_ptr<opt::OptTable> opts =
+      gollvm::options::createGollvmDriverOptTable();
+  CommandLineParser clp(opts.get());
+  if (!clp.parseCommandLine(argc, argv))
+    return 1;
+
+  CompilationOrchestrator orchestrator(argv[0], clp.args(), *opts.get());
+
+  // Collect input files from the command line, resolve target.
+  if (!orchestrator.inspectCommandLine())
     return orchestrator.errorReturnCode();
 
-  // Initialize target and sort out selected command line options.
+  // Initialize target
   if (!orchestrator.preamble())
     return orchestrator.errorReturnCode();
 
