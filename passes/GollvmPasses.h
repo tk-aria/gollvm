af61fe3dc22afe132f52b988c55ea1d9dbf3e2f9
Cherry Zhang <cherryyz@google.com>
2018-09-21 22:29:01 -0400

    passes: add a statepoint insertion pass
    This pass performs liveness analysis and attaches the liveness
    information to statepoints, to support precise stack scan for
    the Go GC. Statepoints are function calls. Currently a
    non-moving GC is implemented.
    
    This pass is based on LLVM's RewriteStatepointsForGC pass, with
    the following modifications:
    
    - We implement a non-moving GC, so gc.relocate is not necessary.
      Related code are removed.
    
    - The original code only tracks live in-register values. For Go,
      the GC roots also include stack allocated objects. So the
      liveness analysis is extended with stack slot (alloca)
      liveness tracking.
    
    - We encode the stack maps in the exception table. To do that,
      we rewrite all the statepoint calls to may-throw invoke, and
      attach the stack map to the landing pad as the type info. For
      each statepoint we attach a symbol go..stackmap.ID, and later
      the stack map generation code will fill in the content of the
      symbol with the actual stack map.
    
    TODO: have some way to unit test
    
    Change-Id: I166135eb9ac753686e6b47d9ae84f7a746fb933f
    Reviewed-on: https://go-review.googlesource.com/c/137761
    Reviewed-by: Than McIntosh <thanm@google.com>

--- passes/GollvmPasses.h
+++ passes/GollvmPasses.h
@@ -9,15 +9,38 @@
 #ifndef LLVM_GOLLVM_PASSES_GOLLVMPASSES_H
 #define LLVM_GOLLVM_PASSES_GOLLVMPASSES_H
 
+#include "llvm/ADT/SmallVector.h"
+
 namespace llvm {
 
-class PassRegistry;
+class DataLayout;
 class FunctionPass;
+class ModulePass;
+class PassRegistry;
+class Type;
+class Value;
 
 void initializeGoAnnotationPass(PassRegistry&);
+void initializeGoStatepointsLegacyPassPass(PassRegistry&);
 
 FunctionPass *createGoAnnotationPass();
+ModulePass *createGoStatepointsLegacyPass();
 
 } // namespace llvm
+
+namespace gollvm {
+namespace passes {
+
+// Helper functions.
+
+// Whether a type contains pointer.
+bool hasPointer(llvm::Type *);
+
+// Compute the pointer bitmap for type T, stored into Words.
+void getPtrBitmapForType(llvm::Type *T, const llvm::DataLayout &DL,
+                         llvm::SmallVectorImpl<llvm::Value *> &Words);
+
+} // namespace passes
+} // namespace gollvm
 
 #endif
