8402d5c07a1d0294c046695f7929bfc628ab721d
Cherry Zhang <cherryyz@google.com>
2019-03-12 15:45:51 -0400

    bridge: make an extra copy when passing an alloca as a byval arg
    When passing an alloca as a byval arg, the LLVM optimizer seems
    to think it uses the address, which prevents it from being SROA'd
    or registerized. Work around this by generating an extra copy,
    which is only used in this call, and the original alloca could be
    SROA'd if otherwise it can be. Currently we only do this when it
    is an alloca (or an offset from it), and the type is small.
    
    Change-Id: If43f9fe6691c64202d7affd483c277a33fa8c926

--- bridge/go-llvm-materialize.cpp
+++ bridge/go-llvm-materialize.cpp
@@ -1094,6 +1094,7 @@ struct GenCallState {
   llvm::SmallVector<llvm::Value *, 16> llargs;
   llvm::Value *chainVal;
   llvm::Value *sretTemp;
+  llvm::SmallVector<llvm::Value *, 1> byvalTemp;
   BFunctionType *calleeFcnType;
   Bfunction *callerFcn;
 
@@ -1175,13 +1176,48 @@ Llvm_backend::genCallMarshallArgs(const std::vector<Bexpression *> &fn_args,
         Bvariable *cv = genVarForConstant(cval, fn_args[idx]->btype());
         val = cv->value();
       }
-      llvm::Type *vt = val->getType();
-      assert(vt->isPointerTy());
-      if (paramInfo.attr() == AttrByVal && vt->getPointerAddressSpace() != 0) {
-        // We pass a stack address, which is always in address space 0.
-        std::string castname(namegen("ascast"));
-        llvm::Type *pt = llvm::PointerType::get(vt->getPointerElementType(), 0);
-        val = builder.CreateAddrSpaceCast(val, pt, castname);
+      if (paramInfo.attr() == AttrByVal) {
+        llvm::Type *vt = val->getType();
+        assert(vt->isPointerTy());
+        llvm::Type *et = vt->getPointerElementType();
+
+        // If we pass an alloca as a byval arg, the LLVM optimizer seems
+        // to think it uses the address, which prevents it from being SROA'd
+        // or registerized.
+        // Work around this by generating an extra copy. We only do this
+        // when it is an alloca (or an offset from it), not known as
+        // address-taken, and the type is small.
+        // FIXME: this really should be the job of the optimizer.
+        if (!fnarg->isConstant() &&
+            vt->getPointerAddressSpace() == 0 &&
+            llvmTypeSize(et) <= 32) {
+          llvm::AllocaInst *ai =
+              llvm::dyn_cast<llvm::AllocaInst>(val->stripInBoundsOffsets());
+          if ((ai && !ai->getMetadata("go_addrtaken"))) {
+            Btype *argTyp = fnarg->btype();
+            std::string tname(namegen("byvaltmp"));
+            llvm::Value *tmpv = state.callerFcn->createTemporary(argTyp, tname);
+            // CreateMemCpy requires a parent block & function.
+            // Use a BlockLIRBuilder with a dummy function.
+            llvm::Function *dummyFcn = errorFunction_->function();
+            BlockLIRBuilder bbuilder(dummyFcn, this);
+            uint64_t sz = llvmTypeSize(et);
+            bbuilder.CreateLifetimeStart(tmpv, bbuilder.getInt64(sz));
+            unsigned align =typeAlignment(argTyp);
+            bbuilder.CreateMemCpy(tmpv, align, val, align, sz);
+            for (auto inst : bbuilder.instructions())
+              builder.Insert(inst);
+            val = tmpv;
+            state.byvalTemp.push_back(tmpv);
+          }
+        }
+
+        if (vt->getPointerAddressSpace() != 0) {
+          // We pass a stack address, which is always in address space 0.
+          std::string castname(namegen("ascast"));
+          llvm::Type *pt = llvm::PointerType::get(et, 0);
+          val = builder.CreateAddrSpaceCast(val, pt, castname);
+        }
       }
       state.llargs.push_back(val);
       continue;
@@ -1331,6 +1367,7 @@ void Llvm_backend::genCallEpilog(GenCallState &state,
 {
   const CABIParamInfo &returnInfo = state.oracle.returnInfo();
 
+  BinstructionsLIRBuilder builder(context_, callExpr);
 
   if (needSretTemp(returnInfo, state.calleeFcnType)) {
     assert(state.sretTemp);
@@ -1342,7 +1379,6 @@ void Llvm_backend::genCallEpilog(GenCallState &state,
       // the expected abstract result type of the function. Cast the
       // sret storage location to a pointer to the abi type and store
       // the ABI return value into it.
-      BinstructionsLIRBuilder builder(context_, callExpr);
       llvm::Type *rt = (returnInfo.abiTypes().size() == 1 ?
                         returnInfo.abiType()  :
                         returnInfo.computeABIStructType(typeManager()));
@@ -1353,6 +1389,18 @@ void Llvm_backend::genCallEpilog(GenCallState &state,
       std::string stname(namegen("st"));
       builder.CreateStore(callInst, bitcast);
     }
+  }
+
+  if (!state.byvalTemp.empty()) {
+    llvm::Function *dummyFcn = errorFunction_->function();
+    BlockLIRBuilder bbuilder(dummyFcn, this);
+    for (llvm::Value *tmpv : state.byvalTemp) {
+      llvm::Type *et = tmpv->getType()->getPointerElementType();
+      uint64_t sz = llvmTypeSize(et);
+      bbuilder.CreateLifetimeEnd(tmpv, bbuilder.getInt64(sz));
+    }
+    for (auto inst : bbuilder.instructions())
+      builder.Insert(inst);
   }
 }
 
