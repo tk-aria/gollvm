196105ce47344576b812d8e25da3dea4f3c04164
Cherry Zhang <cherryyz@google.com>
2019-03-20 13:11:46 -0400

    unittest: fix MakeLocalWithDeclVar test
    The module dump may contain a declaration of the lifetime.start
    instrinsic. Maybe it didn't before, but it does now after I
    update LLVM.
    
    Count actual calls to lifetime.start, not only the function name.
    So it doesn't match the declaration.
    
    Change-Id: I7ab8414127c9ee3b6042291d2b3bc83f988895bb

--- unittests/BackendCore/BackendVarTests.cpp
+++ unittests/BackendCore/BackendVarTests.cpp
@@ -724,7 +724,7 @@ TEST(BackendVarTests, MakeLocalWithDeclVar) {
 
   // Expect to see only one lifetime start, since A) loc1 is at the top
   // level, and B) loc2 uses loc1 as declVar.
-  const char *expected = "@llvm.lifetime.start.p0i8(i64";
+  const char *expected = "call void @llvm.lifetime.start.p0i8(i64";
   EXPECT_EQ(h.countInstancesInModuleDump(expected), 1u);
 }
 
