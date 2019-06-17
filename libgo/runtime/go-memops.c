49655e71ef68f71cab2ad3ca737e45c9e87ea013
Cherry Zhang <cherryyz@google.com>
2019-06-17 17:45:22 -0400

    gollvm: wrap memmove, etc. in nosplit functions
    When memmove (memcpy, memset) is called from a split-stack
    function, the linker will change it to force a split by calling
    __morestack_non_split, because the libc functions are external
    and the linker has no knowledge about them. This may cause
    many unnecessary stack splits.
    
    However, the linker is happy if they are called from a nosplit
    function. So we can work around it by wrapping memmove, etc. in
    nosplit functions.
    
    This CL adds nosplit wrappers functions, __go_memmove, etc.,
    which (tail) calls memmove. Also add a LLVM backend pass, runs at
    the end, changing calls to memmove, etc. to call the wrapper
    functions.
    
    DO NOT SUBMIT
    
    Change-Id: Ic7e39281275ff7da14a8744ee56b4dc57c07321d

---
+++ libgo/runtime/go-memops.c
@@ -0,0 +1,23 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <stddef.h>
+
+void __go_memmove(void*, const void*, size_t) __attribute__((no_split_stack));
+void __go_memmove(void *dst, const void *src, size_t n)
+{
+	__builtin_memmove(dst, src, n);
+}
+
+void __go_memcpy(void*, const void*, size_t) __attribute__((no_split_stack));
+void __go_memcpy(void *dst, const void *src, size_t n)
+{
+	__builtin_memcpy(dst, src, n);
+}
+
+void __go_memset(void*, int, size_t) __attribute__((no_split_stack));
+void __go_memset(void *dst, int c, size_t n)
+{
+	__builtin_memset(dst, c, n);
+}
