ed4e53f089c4e19b8bd5eb56d58c653a8caa7ac5
Cherry Zhang <cherryyz@google.com>
2019-07-15 16:26:25 -0400

    gollvm: intrinsify runtime.getg
    Currently, getg is implemented in C, which loads the thread-local
    g variable. Calling getg from Go, which is somewhat frequent in
    the runtime, is not inlineable. This CL lets the compiler
    generate inlined load for getg. We do this in the middle-end
    instead of the frontend because currently we can only do this
    safely in gollvm, not gccgo.
    
    The concern is that The backend may choose to cache the TLS
    address in a register or on stack. If a thread switch happens,
    the cache will become invalid. Currently, there seems no way to
    tell the backend to disable or invalidate the cache, for both GCC
    and LLVM backend.
    
    However, in LLVM, the caching happens in SelectionDAG, where it
    CSEs the TLS address for multiple loads in the same basic block.
    It does not CSE the TLS address across multiple basic blocks. So
    we are safe if we can ensure there is at most one load of g in a
    block. If there are more than one, we replace it with a call of
    runtime.getg (i.e. undoing the // inlining). We introduce a
    backend pass to do this.
    
    For TLS access in PIC mode, currently LLVM generates assembly
    code that contains detached data16 prefixes, which causes the
    assembler to emit warnings. This doesn't seem to cause any
    actual problems, besides cluttering outputs.
    
    Change-Id: Ib3969470cc838c84f993dece3716399839b69959

--- bridge/go-llvm-materialize.cpp
+++ bridge/go-llvm-materialize.cpp
@@ -1360,6 +1360,25 @@ void Llvm_backend::genCallEpilog(GenCallState &state,
   }
 }
 
+// Inline runtime.getg, generate a load of g.
+// This is not done as a builtin because, unlike other builtins,
+// we need the FE to tell us the result type.
+static llvm::Value *makeGetg(Btype *resType,
+                             BinstructionsLIRBuilder *builder,
+                             Llvm_backend *be)
+{
+  llvm::GlobalValue* g = be->module().getGlobalVariable("runtime.g");
+  if (!g) {
+    bool is_external = true, is_hidden = false, in_unique_section = false;
+    Location location; // dummy
+    Bvariable* bv = be->global_variable("runtime.g", "runtime.g", resType, is_external,
+                                        is_hidden, in_unique_section, location);
+    g = llvm::cast<llvm::GlobalValue>(bv->value());
+    g->setThreadLocal(true);
+  }
+  return builder->CreateLoad(g);
+}
+
 Bexpression *Llvm_backend::materializeCall(Bexpression *callExpr)
 {
   Location location = callExpr->location();
@@ -1461,7 +1480,8 @@ Bexpression *Llvm_backend::materializeCall(Bexpression *callExpr)
       BuiltinExprMaker makerfn = be->exprMaker();
       if (makerfn)
         callValue = makerfn(state.llargs, &state.builder, this);
-    }
+    } else if (fcn->getName() == "runtime.getg")
+      callValue = makeGetg(rbtype, &state.builder, this);
   }
   if (!callValue) {
     llvm::FunctionType *llft =
